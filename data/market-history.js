90aaf5eb8d9a4bebfccf4e9c20a45483bb4c74ea
domizwh-max <domizwh@gmail.com>
2025-11-28 00:19:34 +0630

    Create market-history.js

---
+++ data/market-history.js
@@ -0,0 +1,22 @@
+// Market history data updated at 2025-11-27T12:00:00.000Z
+const marketHistoryData = {
+  "27/11/2025": {
+    "date": "27/11/2025",
+    "dayName": "Thursday",
+    "entries": [
+      {
+        "time": "20:00:00",
+        "set1201": "1,261.23",
+        "value1201": "13,522.14",
+        "lucky1201": "32",
+        "set1630": "1,252.71",
+        "value1630": "23,180.98",
+        "lucky1630": "10"
+      }
+    ]
+  }
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = marketHistoryData;
+}
